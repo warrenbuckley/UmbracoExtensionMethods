a7a8809bb6d407b00d0c979cfb15796963c8645c
Warren Buckley <wbuckley@thecogworks.co.uk>
2013-07-08 10:50:22 +0100

    Initial commit of solution with placeholder classes & a test Umbraco site to test out the extensions

---
+++ ExtensionMethods/Dates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Community.ExtensionMethods
+{
+    public static class Dates
+    {
+    }
+}
---
+++ ExtensionMethods/Strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Community.ExtensionMethods
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Counts number of words in a string
+        /// </summary>
+        /// <param name="str">The string to parse</param>
+        /// <returns>An integer of the number of words found</returns>
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
