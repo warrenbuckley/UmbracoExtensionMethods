046ca73d004f2b718f6129c125c76be569eee6d9
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-07-08 16:40:11 +0100

    Add FormatDateTime extension + some Resharper method renaming

--- ExtensionMethods/Social.cs
+++ ExtensionMethods/Social.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Umbraco.Community.ExtensionMethods.Social
+namespace Umbraco.Community.ExtensionMethods
 {
     public static class Social
     {
