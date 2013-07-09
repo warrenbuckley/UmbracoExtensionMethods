58bec485a9960a6fee34207311edb13376c3f621
Warren Buckley <wbuckley@thecogworks.co.uk>
2013-07-09 09:44:34 +0100

    Update new stubs

--- ExtensionMethods/YouTube.cs
+++ ExtensionMethods/YouTube.cs
@@ -1,1 +1,21 @@
-//todo
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Umbraco.Community.ExtensionMethods.YouTube
+{
+    public static class YouTube
+    {
+
+        //TODO
+        public static string YouTubeEmbed(this string youtubeID, int width, int height)
+        {
+            return string.Empty;
+        }
+        
+    }
+}
