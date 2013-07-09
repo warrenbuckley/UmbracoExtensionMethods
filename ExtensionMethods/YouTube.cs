460fd005237b99475ec909c442c135d69590eabf
Ajmal Vh <ajmalcpp@gmail.com>
2013-07-09 18:56:59 +0530

    Added GetYouTubeId Method.

--- ExtensionMethods/YouTube.cs
+++ ExtensionMethods/YouTube.cs
@@ -16,6 +16,23 @@ namespace Umbraco.Community.ExtensionMethods.YouTube
         {
             return string.Empty;
         }
+
+        /// <summary>
+        /// This will return the ID part of Youtube URLs
+        /// </summary>
+        /// <param name="url">video url</param>
+        /// <returns>video id</returns>
+        public static string GetYoutubeId(string url)
+        {
+            // Example: http://www.youtube.com/watch?v=MD5nU7kt7pg will return MD5nU7kt7pg
+
+            string id = string.Empty;
+            Regex youtubeVideoRegex = new Regex(@"youtu(?:\.be|be\.com)/(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]+)");
+            Match youtubeMatch = youtubeVideoRegex.Match(url);
+            if (youtubeMatch.Success)
+                id = youtubeMatch.Groups[1].Value;
+            return id;
+        }
         
     }
 }
